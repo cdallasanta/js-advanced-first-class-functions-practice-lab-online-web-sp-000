d21309f70192f56a90bc43d89c158872a1cd0363
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-28 16:47:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,16 +14,23 @@ function logDriversByHometown(driverArr, location) {
 };
 
 function driversByRevenue(driverArr) {
-  return driverArr.sort(function(a, b){
+  return driverArr.slice().sort(function(a, b){
     return a.revenue - b.revenue;
   });
 };
 
 function driversByName(driverArr) {
-  return driverArr.sort(function(a, b){
-    return a.localeCompare(b);
+  return driverArr.slice().sort(function(a, b){
+    return a.name.localeCompare(b.name);
   });
 };
 
+
+function totalRevenue(driverArr) {
+  return driverArr.reduce(function(d) {
+    debugger;
+    return d.revenue
+  }, 0);
+};
 // totalRevenue() — Receives an array of driver objects and returns the sum of the revenue earned by each driver.
 // averageRevenue() — Receives an array of driver objects and returns the average revenue earned by each driver.
